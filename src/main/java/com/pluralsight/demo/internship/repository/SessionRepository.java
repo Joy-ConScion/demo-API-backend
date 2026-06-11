39d1680b40f5073df755fa972af543416fba84e7
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-11 11:59:38 -0500

    Temp commenting out code that prevents code from staying up

--- src/main/java/com/pluralsight/demo/internship/repository/SessionRepository.java
+++ src/main/java/com/pluralsight/demo/internship/repository/SessionRepository.java
@@ -1,11 +1,7 @@
-package com.pluralsight.demo.internship.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SessionRepository<SessionEntity> extends JpaRepository<SessionEntity, Long> {
-
-    List<SessionEntity> findByTrackIgnoreCase (String track);
-
-}
+//package com.pluralsight.demo.internship.repository;
+//
+//public interface SessionRepository extends JpaRepository<SessionEntity, Long> {
+//
+//    List<SessionEntity> findByTrackIgnoreCase (String track);
+//
+//}
